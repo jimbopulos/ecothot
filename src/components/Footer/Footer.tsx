7b307bb28c48bddf68fbd59143b2a89f031edf16
jimbopulos <james.mgalantino@gmail.com>
2024-01-14 19:42:10 -0500

    Add basic footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="flex flex-col items-center">
+    <footer className="flex flex-col items-center border-t-2 border-t-red-900 p-2 w-1/2">
       <p>© est 2017 Ecothot LLC. All Rights Reserved.</p>
       <p className="p-2">no time to lose!</p>
     </footer>
