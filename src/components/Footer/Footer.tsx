db2118a66d5a2737b087fdeac1933199f363ac17
jimbopulos <james.mgalantino@gmail.com>
2024-01-14 19:38:43 -0500

    Create and add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="flex flex-col items-center">
+      <p>© est 2017 Ecothot LLC. All Rights Reserved.</p>
+      <p className="p-2">no time to lose!</p>
+    </footer>
+  );
+}
