d86fdbbc5e788ef6a6738a2d52ddc57bec31d76d
James Galantino <james.mgalantino@gmail.com>
2024-01-16 17:04:23 -0500

    Add Footer (#2)

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="flex flex-col items-center text-center text-balance text-xs gap-2 border-t-2 border-t-red-900 p-2 w-4/5">
+      <p className="pt-2">© est 2017 Ecothot LLC. All Rights Reserved.</p>
+      <p className="pb-2 italic">no time to lose!</p>
+    </footer>
+  );
+}
