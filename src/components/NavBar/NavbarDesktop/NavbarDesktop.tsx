a7d5819aeda5bf702ca43f8fc10bad46f60570cf
jimbopulos <james.mgalantino@gmail.com>
2024-01-22 18:05:10 -0500

    Modify desktop nav html, active links

--- src/components/NavBar/NavbarDesktop/NavbarDesktop.tsx
+++ src/components/NavBar/NavbarDesktop/NavbarDesktop.tsx
@@ -1,28 +1,78 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+// add props for links (objects with href, key, etc.)
+// map to cleanup and DRY
 
 export function NavbarDesktop() {
+  const currentPath = usePathname();
+  const isActive = (href: string) => currentPath === href;
+
   return (
-    <nav className="flex font-semibold text-lg rounded-sm justify-center shrink mt-7 px-7 max-w-screen-xl bg-white shadow shadow-black">
-      <ul className="list-none">
-        <li className="inline-block px-5 py-2 mr-7 transition duration-300 hover:text-white hover:bg-zinc-800">
-          <Link href="/">Welcome</Link>
-        </li>
-        <li className="inline-block px-5 py-2 mr-7 transition duration-300 hover:text-white hover:bg-zinc-800">
-          <Link href="https://discord.gg/GSqNxP7D">Discord</Link>
-        </li>
-        <li className="inline-block px-5 py-2 mr-7 transition duration-300 hover:text-white hover:bg-zinc-800">
-          <Link href="/shop">Shop</Link>
-        </li>
-        <li className="inline-block px-5 py-2 mr-7 transition duration-300 hover:text-white hover:bg-zinc-800">
-          <Link href="/games">Games</Link>
-        </li>
-        <li className="inline-block px-5 py-2 mr-7 transition duration-300 hover:text-white hover:bg-zinc-800">
-          <Link href="/loyalty">Points</Link>
-        </li>
-        <li className="inline-block px-5 py-2 transition duration-300 hover:text-white hover:bg-zinc-800">
-          <Link href="/forum">Cybernetic</Link>
-        </li>
-      </ul>
+    <nav className="flex font-semibold text-lg rounded-sm justify-center shrink mt-7 px-9 max-w-screen-xl bg-white shadow shadow-black">
+      <Link
+        href="/"
+        className={`inline-block px-5 py-2 mr-7 ${
+          isActive('/')
+            ? 'text-white bg-zinc-800'
+            : 'transition duration-300 hover:text-white hover:bg-zinc-800'
+        }
+            `}
+      >
+        Welcome
+      </Link>
+      <Link
+        href="https://discord.gg/GSqNxP7D"
+        className="inline-block px-5 py-2 mr-7 transition duration-300 hover:text-white hover:bg-zinc-800"
+      >
+        Discord
+      </Link>
+      <Link
+        href="/shop"
+        className={`inline-block px-5 py-2 mr-7 ${
+          isActive('/shop')
+            ? 'text-white bg-zinc-800'
+            : 'transition duration-300 hover:text-white hover:bg-zinc-800'
+        }
+            `}
+      >
+        Shop
+      </Link>
+      <Link
+        href="/games"
+        className={`inline-block px-5 py-2 mr-7 ${
+          isActive('/games')
+            ? 'text-white bg-zinc-800'
+            : 'transition duration-300 hover:text-white hover:bg-zinc-800'
+        }
+            `}
+      >
+        Games
+      </Link>
+      <Link
+        href="/loyalty"
+        className={`inline-block px-5 py-2 mr-7 ${
+          isActive('/loyalty')
+            ? 'text-white bg-zinc-800'
+            : 'transition duration-300 hover:text-white hover:bg-zinc-800'
+        }
+            `}
+      >
+        Points
+      </Link>
+      <Link
+        href="/forum"
+        className={`inline-block px-5 py-2 ${
+          isActive('/forum')
+            ? 'text-white bg-zinc-800'
+            : 'transition duration-300 hover:text-white hover:bg-zinc-800'
+        }
+            `}
+      >
+        Cybernetic
+      </Link>
     </nav>
   );
 }
