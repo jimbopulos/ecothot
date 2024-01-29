e6f83e908c281be776b4efe8f2b043894e64cdce
jimbopulos <james.mgalantino@gmail.com>
2024-01-28 23:15:44 -0500

    Dry up navbar desktop code etc

--- src/components/NavBar/NavbarDesktop/NavbarDesktop.tsx
+++ src/components/NavBar/NavbarDesktop/NavbarDesktop.tsx
@@ -1,78 +1,38 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-
-// add props for links (objects with href, key, etc.) -- include isFirst/isLast element prop
-// map to cleanup and DRY
+import { useSelectedLayoutSegment, usePathname } from 'next/navigation';
+import { navItems } from '@/utils';
 
 export function NavbarDesktop() {
   const currentPath = usePathname();
-  const isActive = (href: string) => currentPath === href;
+  // const segment = useSelectedLayoutSegment();
 
   return (
-    <nav className="flex flex-row font-semibold text-lg rounded-sm justify-center mt-7 px-18 max-w-screen-xl bg-white shadow-md shadow-gray-800">
-      <Link
-        href="/"
-        className={` px-6 py-2 mr-7 ${
-          isActive('/')
-            ? 'text-white bg-zinc-800 rounded-l-sm'
-            : 'transition duration-200 hover:text-white hover:bg-zinc-800 hover:rounded-l-sm'
-        }
-            `}
-      >
-        Welcome
-      </Link>
-      <Link
-        href="https://discord.gg/GSqNxP7D"
-        className=" px-6 py-2 mr-7 transition duration-200 hover:text-white hover:bg-zinc-800"
-      >
-        Discord
-      </Link>
-      <Link
-        href="/shop"
-        className={` px-6 py-2 mr-7 ${
-          isActive('/shop')
-            ? 'text-white bg-zinc-800'
-            : 'transition duration-200 hover:text-white hover:bg-zinc-800'
-        }
-            `}
-      >
-        Shop
-      </Link>
-      <Link
-        href="/games"
-        className={` px-6 py-2 mr-7 ${
-          isActive('/games')
-            ? 'text-white bg-zinc-800'
-            : 'transition duration-200 hover:text-white hover:bg-zinc-800'
-        }
-            `}
-      >
-        Games
-      </Link>
-      <Link
-        href="/loyalty"
-        className={` px-6 py-2 mr-7 ${
-          isActive('/loyalty')
-            ? 'text-white bg-zinc-800'
-            : 'transition duration-200 hover:text-white hover:bg-zinc-800'
-        }
-            `}
-      >
-        Points
-      </Link>
-      <Link
-        href="/forum"
-        className={` px-6 py-2 ${
-          isActive('/forum')
-            ? 'text-white bg-zinc-800 rounded-r-sm'
-            : 'transition duration-200 hover:text-white hover:bg-zinc-800 hover:rounded-r-sm'
-        }
-            `}
-      >
-        Cybernetic
-      </Link>
+    <nav className="hidden lg:flex flex-row font-semibold text-lg rounded-sm justify-center mt-7 px-18 max-w-screen-xl bg-white shadow-md shadow-gray-800">
+      {navItems.map(({ id, title, href, isFirstItem, isLastItem }) => {
+        const isActive = currentPath === href;
+
+        return (
+          <Link
+            key={id}
+            href={href}
+            className={
+              isActive
+                ? `px-6 py-2 ${!isLastItem && 'mr-7'} text-white bg-zinc-800 ${
+                    isFirstItem && 'rounded-l-sm'
+                  } ${isLastItem && 'rounded-r-sm'}`
+                : `px-6 py-2 ${
+                    !isLastItem && 'mr-7'
+                  } transition duration-200 hover:text-white hover:bg-zinc-800 ${
+                    isFirstItem && 'hover:rounded-l-sm'
+                  } ${isLastItem && 'hover:rounded-r-sm'}`
+            }
+          >
+            {title}
+          </Link>
+        );
+      })}
     </nav>
   );
 }
