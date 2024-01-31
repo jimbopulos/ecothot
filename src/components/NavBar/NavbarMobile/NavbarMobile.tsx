b361b6523f9aa799cac44bdb39eddafefdf3add0
jimbopulos <james.mgalantino@gmail.com>
2024-01-30 19:25:48 -0500

    Remove some unused code

--- src/components/NavBar/NavbarMobile/NavbarMobile.tsx
+++ src/components/NavBar/NavbarMobile/NavbarMobile.tsx
@@ -54,46 +54,6 @@ export function NavbarMobile() {
           })}
         </div>
       ) : (
-        // <div
-        //   className={`transition-all duration-300 ease-out ${
-        //     isOpen
-        //       ? 'flex flex-col w-full h-lvh left-0 z-10 justify-evenly items-center bg-white rounded-sm shadow-md shadow-gray-800'
-        //       : 'hidden'
-        //   }
-        //   `}
-        // >
-        //   {/* <div className="absolute top-0 right-0 px-8 py-8" onClick={handleClick}>
-        //   <svg
-        //     className="h-8 w-8 text-red-900"
-        //     viewBox="0 0 24 24"
-        //     fill="none"
-        //     stroke="currentColor"
-        //     strokeWidth="2"
-        //     strokeLinecap="round"
-        //     strokeLinejoin="round"
-        //   >
-        //     <line x1="18" y1="6" x2="6" y2="18" />
-        //     <line x1="6" y1="6" x2="18" y2="18" />
-        //   </svg>
-        // </div> */}
-        //   {navItems.map(({ id, title, href, isFirstItem, isLastItem }) => {
-        //     const isActive = currentPath === href;
-
-        //     return (
-        //       <Link
-        //         key={id}
-        //         href={href}
-        //         className={`${
-        //           isFirstItem && 'mt-4'
-        //         } px-12 py-4 rounded-sm text-3xl sm:w-3/4 min-w-fit text-center ${
-        //           isActive && 'text-white bg-zinc-800'
-        //         }`}
-        //       >
-        //         {title}
-        //       </Link>
-        //     );
-        //   })}
-        // </div>
         ''
       )}
     </nav>
