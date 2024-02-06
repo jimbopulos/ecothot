4174539e0d504d8d5878162c20e0333e9fd22324
James Galantino <james.mgalantino@gmail.com>
2024-02-05 22:32:05 -0500

    Update Navbar Responsiveness for Mobile (hamburger) (#3)

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,9 +1,9 @@
-import { NavBar, Footer } from '@/components';
+import { Navbar, Footer } from '@/components';
 
 export default function Games() {
   return (
     <main className="flex flex-col min-h-screen items-center">
-      <NavBar />
+      <Navbar />
       <div className="h-full p-20">This is the Games page</div>
       <Footer />
     </main>
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,11 @@
+import { NavbarDesktop } from './NavbarDesktop';
+import { NavbarMobile } from './NavbarMobile';
+
+export function Navbar() {
+  return (
+    <>
+      <NavbarDesktop />
+      <NavbarMobile />
+    </>
+  );
+}
