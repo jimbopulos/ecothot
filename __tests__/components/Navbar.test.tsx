5d7080384156a53ed609a01b588a05bd4d3823a0
jimbopulos <james.mgalantino@gmail.com>
2024-03-07 12:12:40 -0500

    Refactor for more coverage for NavbarDesktop tests

--- __tests__/components/Navbar.test.tsx
+++ __tests__/components/Navbar.test.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from '@/components';
+import { NavbarDesktop } from '@/components/Navbar/NavbarDesktop';
 import { NavbarMobile } from '@/components/Navbar/NavbarMobile';
 import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
@@ -12,7 +13,15 @@ describe('component module', () => {
     expect(component).toBeInTheDocument();
   });
 
-  test('should open navigation window when hamburger is clicked', async () => {
+  test('should render in Desktop view', async () => {
+    render(<NavbarDesktop />);
+
+    const component = screen.getByTestId('test-NavbarDesktop');
+
+    expect(component).toBeInTheDocument();
+  });
+
+  test('should open navigation window when hamburger is clicked in Mobile view', async () => {
     render(<NavbarMobile />);
 
     const buttonElement = screen.getByRole('button');
