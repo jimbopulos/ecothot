9bb15015bc5825f5c137291a22c58ae518f5c9af
James Galantino <james.mgalantino@gmail.com>
2024-03-08 11:57:46 -0500

    Develop --> Main

--- src/app/loyalty/page.tsx
+++ src/app/loyalty/page.tsx
@@ -1,9 +1,9 @@
-import { NavBar, Footer } from '@/components';
+import { Navbar, Footer } from '@/components';
 
 export default function Loyalty() {
   return (
     <main className="flex flex-col min-h-screen items-center">
-      <NavBar />
+      <Navbar />
       <div className="h-full p-20">This is the Points page</div>
       <Footer />
     </main>
---
+++ src/components/NavBar/NavbarMobile/NavbarMobile.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { navItems } from '@/utils';
+import { useState } from 'react';
+
+export function NavbarMobile() {
+  const currentPath = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+
+  return (
+    <nav
+      className="lg:hidden flex flex-col absolute w-full left-0 font-semibold p-2.5"
+      data-testid="test-NavbarMobile"
+    >
+      <button
+        onClick={handleClick}
+        className={`flex flex-col absolute justify-center items-center p-2.5 transition-all duration-300 ease-out bg-white rounded-md ${
+          !isOpen && 'shadow-md shadow-gray-800'
+        }`}
+      >
+        <span
+          className={`block h-1 w-7 bg-red-900 transition-all duration-300 ease-out rounded-full ${
+            isOpen ? 'rotate-45 translate-y-2' : '-translate-y-0.5'
+          }`}
+        ></span>
+        <span
+          className={`block h-1 w-7 bg-red-900 transition-all duration-300 ease-out rounded-full my-1 ${
+            isOpen ? 'opacity-0' : 'opacity-100'
+          }`}
+        ></span>
+        <span
+          className={`block h-1 w-7 bg-red-900 transition-all duration-300 ease-out rounded-full ${
+            isOpen ? '-rotate-45 -translate-y-2' : 'translate-y-0.5'
+          }`}
+        ></span>
+      </button>
+
+      {isOpen ? (
+        <div
+          className="flex flex-col w-full h-lvh justify-evenly items-center bg-white rounded-sm shadow-md shadow-gray-800 transition-all duration-300 ease-out"
+          data-testid="test-NavbarMobile-open"
+        >
+          {navItems.map(({ id, title, href }) => {
+            const isActive = currentPath === href;
+
+            return (
+              <Link
+                key={id}
+                href={href}
+                className={`px-12 py-4 rounded-sm shadow-lg shadow-gray-800 text-3xl w-3/5 min-w-fit text-center ${
+                  isActive && 'text-white bg-zinc-800'
+                }`}
+              >
+                {title}
+              </Link>
+            );
+          })}
+        </div>
+      ) : (
+        ''
+      )}
+    </nav>
+  );
+}
