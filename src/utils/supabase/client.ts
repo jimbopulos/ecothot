a049c5ed018bcec26a423bb41849a22fd8ccd603
jimbopulos <james.mgalantino@gmail.com>
2024-04-13 21:15:33 -0400

    Add supabase dir and subsequent setup client files

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,9 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-export const createClient = () =>
+const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+
+export default createClient;
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { type NextRequest, NextResponse } from 'next/server';
 
-export const createClient = (request: NextRequest) => {
+const createClient = (request: NextRequest) => {
   // Create an unmodified response
   let response = NextResponse.next({
     request: {
@@ -59,3 +59,5 @@ export const createClient = (request: NextRequest) => {
 
   return { supabase, response };
 };
+
+export default createClient;
